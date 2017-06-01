e8dc06c4aa62c4600e661a18b30ff90064fe2923
ganesa <ganeshaghav12@gmail.com>
2017-06-01 14:19:32 +0530

    first commit

---
+++ app/src/main/java/ganesh/com/bootstrapwebsite/MainActivity.java
@@ -0,0 +1,23 @@
+package ganesh.com.bootstrapwebsite;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView;
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/car/index.html");
+
+    }
+}
